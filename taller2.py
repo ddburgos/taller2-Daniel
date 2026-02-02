3e340a85ce78d411c9635fd12ff78b8badf0a95e
Daniel <dd.burgos@uniandes.edu.co>
2026-02-02 00:39:51 -0500

    commit3: se agrega cambio para Github

--- taller2.py
+++ taller2.py
@@ -5,3 +5,5 @@ x1 = x*2
 print("A este archivo se agrega x1=", x1)
 x2 = 3*(2*x1+1)
 print("Se hace este cambio en esta rama x2=", x2)
+x3 = x2**2+2*x1
+print("Se hace este cambio para subir a Github: x3 =", x3)
