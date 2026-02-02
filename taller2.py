55a88a9f8506d2fc7f3d44bfef886ae01cfa4223
l-munozc <l.munozc@uniandes.edu.co>
2026-02-02 18:06:23 -0500

    Compañero realiza cambio en el repositorio en rama "Lucas_modificación_B1". Se calcula el promedio de los 3 numeros previamente calculados.

--- taller2.py
+++ taller2.py
@@ -7,3 +7,5 @@ x2 = 3*(2*x1+1)
 print("Se hace este cambio en esta rama x2=", x2)
 x3 = x2**2+2*x1
 print("Se hace este cambio para subir a Github: x3 =", x3)
+prom = (x1+x2+x3)/3
+print(f"Se modifica el archivo por el compañero en una rama. El promedio = {prom}")
